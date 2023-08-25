436ba1e6be7f82636ec96add9d4dd430ac931dd8
Joan <jeipige@gmail.com>
2023-08-25 15:02:17 -0500

    Integrating L. API with delivery-status-transport

--- lib/helpers/server.dart
+++ lib/helpers/server.dart
@@ -1,10 +1,10 @@
 // String generalServer = "http://localhost:1337";
-// String generalServerApiLaravel =
+String generalServerApiLaravel ="http://localhost:8000";
 //     "https://easyecommercelaravel-production.up.railway.app";
 
-// String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
+String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
 
 String generalServer = "https://api.easyecomerce.com";
-String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
-String generalServerApiLaravel =
-    "https://easyecommercelaravel-production.up.railway.app";
+// String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
+// String generalServerApiLaravel =
+    // "https://easyecommercelaravel-production.up.railway.app";
