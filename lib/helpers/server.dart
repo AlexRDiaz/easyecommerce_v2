b19927f3b04b7e1b7609f29af8f97d5436056d42
AlexRDiaz <bugi2532@hotmail.com>
2023-08-25 15:28:23 -0500

    added new commit

--- lib/helpers/server.dart
+++ lib/helpers/server.dart
@@ -1,10 +1,4 @@
-// String generalServer = "http://localhost:1337";
-// String generalServerApiLaravel =
-//     "https://easyecommercelaravel-production.up.railway.app";
+String generalServer = "http://localhost:1337";
+String generalServerApiLaravel ="http://localhost:8000";
+String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
 
-// String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
-
-String generalServer = "https://api.easyecomerce.com";
-String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
-String generalServerApiLaravel =
-    "https://easyecommercelaravel-production.up.railway.app";
