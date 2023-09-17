7edb44d922a882eed278f9eff4d5d871a27509d3
Dianac <dcarlosamam@gmail.com>
2023-09-17 09:59:28 -0500

    added sellers my account laravel version 2

--- lib/helpers/server.dart
+++ lib/helpers/server.dart
@@ -1,9 +1,8 @@
+String generalServer = "https://api.easyecomerce.com";
+String generalServerApiLaravel = "http://localhost:8000";
+String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
 
 // String generalServer = "https://api.easyecomerce.com";
-//String generalServerApiLaravel = "http://localhost:8000";
-//String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
-
-String generalServer = "https://api.easyecomerce.com";
-String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
-String generalServerApiLaravel =
-    "https://easyecommercelaravel-production.up.railway.app";
+// String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
+// String generalServerApiLaravel =
+//     "https://easyecommercelaravel-production.up.railway.app";
