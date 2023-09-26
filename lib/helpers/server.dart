8b6433093beb7160a4b46ad80f74459b4d947bad
AlexRDiaz <bugi2532@hotmail.com>
2023-09-26 18:32:50 -0500

    added all

--- lib/helpers/server.dart
+++ lib/helpers/server.dart
@@ -1,9 +1,9 @@
-// String generalServer = "http://localhost:1337";
-// String generalServerApiLaravel = "http://localhost:8000";
-// String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
+String generalServer = "http://localhost:1337";
+String generalServerApiLaravel = "http://localhost:8000";
+String serverUrlByShopify = "http://localhost:1337/api/shopify/pedidos";
 
-String generalServer = "https://api.easyecomerce.com";
-String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
-String generalServerApiLaravel =
-    "https://devapi.easyecomerce.com/apitest/public/index.php";
-//
+// String generalServer = "https://api.easyecomerce.com";
+// String serverUrlByShopify = "https://api.easyecomerce.com/api/orders/post";
+// String generalServerApiLaravel =
+//     "https://devapi.easyecomerce.com/apitest/public/index.php";
+// //
