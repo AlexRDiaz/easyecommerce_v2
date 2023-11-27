ce78af8d2d793d6f27a76c525c9c97c8182b3ad6
Joan <jeipige@gmail.com>
2023-11-27 09:10:39 -0500

    delete cupertino unused imports

--- lib/ui/operator/resolved_novelties/resolved_novelties_info.dart
+++ lib/ui/operator/resolved_novelties/resolved_novelties_info.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:frontend/config/colors.dart';
 
 import 'package:frontend/connections/connections.dart';
