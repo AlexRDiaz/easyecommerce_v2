1168a18935bc0790b1d57dc71ecc7d5244fa9896
Joan <jeipige@gmail.com>
2024-02-19 11:27:27 -0500

    update version

--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -238,7 +238,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 30,
             ),
             const Text(
-              "EASYECOMMERCE - Copyright © 2023.  v.2.2.17",
+              "EASYECOMMERCE - Copyright © 2023.  v.2.2.18",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
           ],
