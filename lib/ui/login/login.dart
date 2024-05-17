c8788c3be574d112d7c0bc5e45107a248836a8f5
DianaC <dcarlosamam@gmail.com>
2024-05-17 07:15:05 -0500

    added label

--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -238,7 +238,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 30,
             ),
             const Text(
-              "EASYECOMMERCE - Copyright © 2023.  v.3.0.0",
+              "EASYECOMMERCE - Copyright © 2023.  v.3.0.1",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
           ],
